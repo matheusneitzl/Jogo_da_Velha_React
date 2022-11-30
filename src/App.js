99982a8ef9b620ea7df447d4f92fe564ee7bdcb1
Matheus Neitzl <matheus.neitzl@gmail.com>
2022-11-29 23:16:21 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Game from './Game';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Jogo da velha REACTJS</h1>
+      <Game />
+    </div>
+  );
+}
